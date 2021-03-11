fc553e6c11cc4ada4de0df4bae2710ff392cfc08
Dspsyuodd <varlamovartem1111@gmail.com>
2021-03-11 19:58:12 +0400

    sd

--- PIxelart/Assets/Script/CameraController.cs
+++ PIxelart/Assets/Script/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     private bool isMouseDown = false;
     private float mouse_sens = 0.35f;
-    private float mouse_wheel_sesn = 200f;
+    private float mouse_wheel_sesn = 2f;
 
     void Update()
     {
@@ -20,6 +20,7 @@ public class CameraController : MonoBehaviour
             transform.position -= new Vector3(Input.GetAxis("Mouse X") * mouse_sens * Mathf.Abs(transform.position.z / 5), Input.GetAxis("Mouse Y") * mouse_sens * Mathf.Abs(transform.position.z / 5), 0f);
 
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
-            transform.position += transform.forward * Time.deltaTime * Input.GetAxis("Mouse ScrollWheel") * mouse_wheel_sesn * Mathf.Abs(transform.position.z / 2);
+            GetComponent<Camera>().orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * mouse_wheel_sesn * Mathf.Abs(GetComponent<Camera>().orthographicSize / 3);
+            //transform.position += transform.forward * Time.deltaTime * Input.GetAxis("Mouse ScrollWheel") * mouse_wheel_sesn * Mathf.Abs(transform.position.z / 2);
     }
 }
