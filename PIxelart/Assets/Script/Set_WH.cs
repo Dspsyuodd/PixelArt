4f1b80fadab1e4ad733afe66496ee7b8e6181806
hypevade <schkalka@gmail.com>
2021-03-12 16:32:37 +0300

    Set_value

---
+++ PIxelart/Assets/Script/Set_WH.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Set_WH : MonoBehaviour
+{
+    public static int Height;
+    public static int Width;
+    public InputField Wi;
+    public InputField He;
+
+    public void Set_value()
+    {
+        Height = int.Parse(He.text.ToString());
+        Width = int.Parse(Wi.text.ToString());
+        print(He.text.ToString());
+        print(Wi.text.ToString());
+    } 
+}
